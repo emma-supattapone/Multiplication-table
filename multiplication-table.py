2d6a59002a0e358c5a429730587665a0397c1b8a
emma-supattapone <emmasupattapone@hanovernorwichschools.org>
2017-02-14 14:16:24 -0500

    Updated from Brython Server: 2/14/2017 2:16:33 PM

--- multiplication-table.py
+++ multiplication-table.py
@@ -21,3 +21,7 @@ Height of multiplication table: 8
   7  14  21  28  35  42  49  56  63  70
   8  16  24  32  40  48  56  64  72  80
 """
+w = input("Width of multiplication table: ")
+h = input("Height of multiplication table: "
+for i in range(w):
+   print(i)
